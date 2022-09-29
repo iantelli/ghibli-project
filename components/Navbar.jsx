b4572bbcad25f616f4d7b452fb98914cf4fb7d03
Ian Chao <90526260+iantelli@users.noreply.github.com>
2022-09-29 15:33:21 -0700

    updated single content page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,21 +7,37 @@ export default function Navbar() {
       <div className="bg-gray-800">
         <div className="h-16 mx-auto px-5 flex items-center justify-between">
           <Link
-            className="text-2xl hover:text-cyan-500 transition-colors cursor-pointer"
-            href="/"
+            className="text-2xl transition-colors cursor-pointer"
+            href="/films"
           >
-            <Image src="/logo.png" alt="logo" width="50" height="60" />
+            <Image src="/logo.png" alt="logo" width="40" height="50" />
           </Link>
 
           <ul className="flex items-center gap-5">
             <li>
-              <Link classNameName="hover:text-cyan-500 transition-colors" href="/">
+              <Link
+                classNameName="hover:text-cyan-500 transition-colors"
+                href="/"
+              >
                 Home
               </Link>
             </li>
             <li>
-              <Link className="hover:text-cyan-500 transition-colors" href="/films">
+              <Link
+                className="hover:text-cyan-500 transition-colors"
+                href="/films"
+              >
                 Films
+              </Link>
+            </li>
+            <li>
+              <Link href="https://github.com/iantelli">
+                <Image
+                  src="/githubLogo.png"
+                  alt="GitHub Logo"
+                  width="30"
+                  height="30"
+                />
               </Link>
             </li>
           </ul>
--- pages/films/[id].js
+++ pages/films/[id].js
@@ -1,22 +1,94 @@
 import axios from "axios";
-import styles from "../../styles/Home.module.css";
 
 export default function Film({ film }) {
-  console.log(film);
+  const styles = {
+    green: {
+      color: "bg-green-500 py-1 px-2 rounded text-white text-sm",
+    },
+    red: {
+      color: "bg-red-500 py-1 px-2 rounded text-white text-sm",
+    },
+    yellow: {
+      color: "bg-yellow-500 py-1 px-2 rounded text-white text-sm",
+    },
+  };
 
   return (
-    <div className="container mx-auto">
-      <div className="flex">
-        <div className="flex-1">
-          <img src={film.image} alt={film.title} />
+    <div className="container mx-auto my-5 p-5">
+      <div className="md:flex no-wrap md:-mx-2">
+        <div className="w-full md:w-3/12 md:mx-2">
+          <div className="bg-gray-800 p-3 rounded-lg shadow-xl">
+            <div className="image overflow-hidden">
+              <img
+                className="h-auto w-full mx-auto"
+                src={film.image}
+                alt={film.title}
+              />
+            </div>
+          </div>
+          <div className="my-4" />
+          <div className="bg-gray-800 p-3 rounded-lg shadow-xl">
+            <h1 class="text-gray-100 font-bold text-xl leading-8 my-1">
+              {film.title}
+            </h1>
+            <h3 className="text-gray-300 font-lg text-semibold leading-6">
+              Original Title: {film.original_title} <br />
+              Romanized Title: {film.original_title_romanised} <br />
+              Release Year: {film.release_date} <br />
+              Running Time: {film.running_time} minutes <br />
+            </h3>
+            <ul class="bg-gray-600 text-gray-300 hover:text-gray-400 hover:shadow py-2 px-3 mt-3 divide-y rounded shadow-sm">
+              <li class="flex items-center py-3">
+                <span>Score</span>
+                <span class="ml-auto">
+                  <span
+                    className={
+                      film.rt_score <= 30
+                        ? styles.red.color
+                        : film.rt_score <= 70
+                        ? styles.yellow.color
+                        : styles.green.color
+                    }
+                  >
+                    {film.rt_score}
+                  </span>
+                </span>
+              </li>
+              <li class="flex items-center py-3">
+                <span>Director: {film.director}</span> <br />
+                <span>Producer: {film.producer}</span>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="flex-1">
-          <h1 className={styles.title}>{film.title}</h1>
-          <p className={styles.description}>{film.description}</p>
+        <div className="w-full md:w-9/12 mx-2 h-64">
+          <div className="bg-gray-800 p-3 rounded-lg shadow-xl divide-y">
+            <div>
+              <p>
+                <span class="text-gray-100 font-bold text-xl leading-8 my-6">
+                  Synposis:
+                </span>
+              </p>
+            </div>
+            <div>
+              <h1 class="text-gray-100 font-lg text-lg leading-8 my-4">
+                {film.description}
+              </h1>
+              <p>
+                <span class="text-gray-100 font-bold text-xl leading-8 my-4">
+                  Movie Banner:
+                </span>
+              </p>
+            </div>
+            <div>
+              <img
+                className="h-auto w-full mx-auto my-4"
+                src={film.movie_banner}
+                alt={film.title}
+              />
+            </div>
+          </div>
         </div>
-        <p> {film.rt_score} </p>
-        <p>{film.director}</p>
-        
       </div>
     </div>
   );
